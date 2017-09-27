52048e4a5ef3b51f9b36cff3d806d288af3879e3
ADRIÁN MONTOYA ROS <amr139@clL18-6>
2017-09-27 10:05:52 +0200

    Actualizando Usuario con apellidos y fecha nacimiento

--- app/models/Usuario.java
+++ app/models/Usuario.java
@@ -1,11 +1,16 @@
 package models;
 
+import java.util.Date;
+
 public class Usuario {
     public String login;
     public String email;
     public String password;
     public String nombre;
-  
+    public String apellidos;
+    public Date fechaNacimiento;
+
+
 
   public Usuario(String login, String email){
     this.login = login;
