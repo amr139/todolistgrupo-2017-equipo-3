af42db4dc0f0a71aac2f2795a39e0530b57ff4c1
= <trabajosadrianmr@gmail.com>
2017-09-29 00:16:34 +0200

    Versión inicial registro usuario

--- app/services/UsuarioService.java
+++ app/services/UsuarioService.java
@@ -1,11 +1,16 @@
 package services;
 
+import javax.inject.*;
+
 import models.Usuario;
 import models.UsuarioRepository;
 
 public class UsuarioService {
   UsuarioRepository repository;
 
+  // Play proporcionará automáticamente el UsuarioRepository necesario
+  // usando inyección de dependencias
+  @Inject
   public UsuarioService(UsuarioRepository repository){
     this.repository = repository;
   }
