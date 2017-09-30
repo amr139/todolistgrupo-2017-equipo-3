87cc5360074bf9e424c356b7b19fd78599ea261e
= <trabajosadrianmr@gmail.com>
2017-09-30 12:22:32 +0200

    Vista y controlador detalle de usuario

--- app/controllers/UsuarioController.java
+++ app/controllers/UsuarioController.java
@@ -6,57 +6,69 @@ import views.html.*;
 import javax.inject.*;
 import play.data.Form;
 import play.data.FormFactory;
+import play.Logger;
 
 import services.UsuarioService;
 import models.Usuario;
 
+
 public class UsuarioController extends Controller {
 
-  @Inject FormFactory formFactory;
+   @Inject FormFactory formFactory;
 
-  // Play injecta un usuarioService junto con todas las dependencias necesarias:
-  // UsuarioRepository y JPAApi
-  @Inject UsuarioService usuarioService;
+   // Play injecta un usuarioService junto con todas las dependencias necesarias:
+   // UsuarioRepository y JPAApi
+   @Inject UsuarioService usuarioService;
 
-  public Result saludo(String mensaje) {
+   public Result saludo(String mensaje) {
       return ok(saludo.render("El mensaje que he recibido es: " + mensaje));
-  }
+   }
 
-  public Result formularioRegistro(){
-    return ok(formRegistro.render(formFactory.form(Registro.class),""));
-  }
+   public Result formularioRegistro() {
+      return ok(formRegistro.render(formFactory.form(Registro.class),""));
+   }
 
-  public Result registroUsuario(){
-    Form<Registro> form = formFactory.form(Registro.class).bindFromRequest();
-    if(form.hasErrors()){
-      return badRequest(formRegistro.render(form,"Hay errores en el formulario"));
-    }
-    Registro datosRegistro = form.get();
+   public Result registroUsuario() {
+      Form<Registro> form = formFactory.form(Registro.class).bindFromRequest();
+      if (form.hasErrors()) {
+         return badRequest(formRegistro.render(form, "Hay errores en el formulario"));
+      }
+      Registro datosRegistro = form.get();
 
-    if(usuarioService.findUsuarioPorLogin(datosRegistro.login) != null) {
-      return badRequest(formRegistro.render(form, "Login ya existende: escoge otro"));
-    }
+      if (usuarioService.findUsuarioPorLogin(datosRegistro.login) != null) {
+         return badRequest(formRegistro.render(form, "Login ya existente: escoge otro"));
+      }
 
-    if(!datosRegistro.password.equals(datosRegistro.confirmacion)) {
-      return badRequest(formRegistro.render(form,"No coinciden la contraseña y la confirmación"));
-    }
-    Usuario usuario = usuarioService.creaUsuario(datosRegistro.login, datosRegistro.email, datosRegistro.password);
-    return ok(saludo.render("Creado " + usuario.toString()));
-  }
+      if (!datosRegistro.password.equals(datosRegistro.confirmacion)) {
+         return badRequest(formRegistro.render(form, "No coinciden la contraseña y la confirmación"));
+      }
+      Usuario usuario = usuarioService.creaUsuario(datosRegistro.login, datosRegistro.email, datosRegistro.password);
+      return ok(saludo.render("Creado " + usuario.toString()));
+   }
 
-  public Result formularioLogin(){
-    return ok(formLogin.render(formFactory.form(Login.class),""));
-  }
+   public Result formularioLogin() {
+      return ok(formLogin.render(formFactory.form(Login.class),""));
+   }
 
-  public Result loginUsuario() {
-    Form<Login> form = formFactory.form(Login.class).bindFromRequest();
-    if(form.hasErrors()){
-      return badRequest(formLogin.render(form,"Hay errores en el formulario"));
-    }
-    Login login = form.get();
-    Usuario usuario = usuarioService.login(login.username,login.password);
-    if(usuario == null){
-      return notFound(formLogin.render(form,"Login y constraseña no existentes"));
-    } else return ok(saludo.render("Logeado " + usuario.toString()));
-  }
+   public Result loginUsuario() {
+      Form<Login> form = formFactory.form(Login.class).bindFromRequest();
+      if (form.hasErrors()) {
+         return badRequest(formLogin.render(form, "Hay errores en el formulario"));
+      }
+      Login login = form.get();
+      Usuario usuario = usuarioService.login(login.username, login.password);
+      if (usuario == null) {
+         return notFound(formLogin.render(form, "Login y contraseña no existentes"));
+      } else return ok(saludo.render("Logeado " + usuario.toString()));
+   }
+
+   public Result detalleUsuario(Long id) {
+      Usuario usuario = usuarioService.findUsuarioPorId(id);
+      if (usuario == null) {
+           return notFound("Usuario no encontrado");
+      } else {
+           Logger.debug("Encontrado usuario " + usuario.getId() + ": " + usuario.getLogin());
+           return ok(detalleUsuario.render(usuario));
+      }
+   }
 }
