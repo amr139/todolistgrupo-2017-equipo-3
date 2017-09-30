5abb0eadda2f2061d4f69aaf19d4319049b88f0b
= <trabajosadrianmr@gmail.com>
2017-09-30 15:54:45 +0200

    Añadido método findAllTareas(idUsuario)

--- app/models/TareaRepository.java
+++ app/models/TareaRepository.java
@@ -1,9 +1,12 @@
 package models;
 
+import java.util.List;
+
 import com.google.inject.ImplementedBy;
 
 @ImplementedBy(JPATareaRepository.class)
 public interface TareaRepository {
   Tarea add(Tarea tarea);
   Tarea findById(Long idTarea);
+  List<Tarea> findAllTareas(Long idUsuario);
 }
