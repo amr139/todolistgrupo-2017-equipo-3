aefcf4e998a1e14d481276493dd83cfd47fff6b9
= <trabajosadrianmr@gmail.com>
2017-11-18 15:47:58 +0100

    Refactor de la vista tableros -> mios/otros

--- app/controllers/GestionTablerosController.java
+++ app/controllers/GestionTablerosController.java
@@ -21,85 +21,98 @@ import models.Tablero;
 import security.ActionAuthenticator;
 
 public class GestionTablerosController extends Controller {
-  @Inject FormFactory formFactory;
-  @Inject UsuarioService usuarioService;
-  @Inject TableroService tableroService;
+    @Inject FormFactory formFactory;
+    @Inject UsuarioService usuarioService;
+    @Inject TableroService tableroService;
 
-  @Security.Authenticated(ActionAuthenticator.class)
-  public Result formularioNuevoTablero(Long idUsuario) {
-    String connectedUserStr = session("connected");
-    Long connectedUser =  Long.valueOf(connectedUserStr);
-    if (connectedUser != idUsuario) {
-       return unauthorized("Lo siento, no estás autorizado");
-    } else {
-       Usuario usuario = usuarioService.findUsuarioPorId(idUsuario);
-       return ok(formNuevoTablero.render(usuario, formFactory.form(Tablero.class),""));
+    @Security.Authenticated(ActionAuthenticator.class)
+    public Result formularioNuevoTablero(Long idUsuario) {
+        String connectedUserStr = session("connected");
+        Long connectedUser =  Long.valueOf(connectedUserStr);
+        if (connectedUser != idUsuario) {
+            return unauthorized("Lo siento, no estás autorizado");
+        } else {
+            Usuario usuario = usuarioService.findUsuarioPorId(idUsuario);
+            return ok(formNuevoTablero.render(usuario, formFactory.form(Tablero.class),""));
+        }
     }
-  }
 
-  @Security.Authenticated(ActionAuthenticator.class)
-  public Result creaNuevoTablero(Long idUsuario) {
-    String connectedUserStr = session("connected");
-    Long connectedUser =  Long.valueOf(connectedUserStr);
-    if (connectedUser != idUsuario) {
-       return unauthorized("Lo siento, no estás autorizado");
-    } else {
-       Form<Tablero> tableroForm = formFactory.form(Tablero.class).bindFromRequest();
-       if (tableroForm.hasErrors()) {
-          Usuario usuario = usuarioService.findUsuarioPorId(idUsuario);
-          return badRequest(formNuevoTablero.render(usuario, formFactory.form(Tablero.class), "Hay errores en el formulario"));
-       }
-       Tablero tablero = tableroForm.get();
-       tableroService.nuevoTablero(idUsuario, tablero.getNombre());
-       flash("aviso","El tablero se ha grabado correctamente");
-       return redirect(controllers.routes.GestionTablerosController.listarTableros(idUsuario));
+    @Security.Authenticated(ActionAuthenticator.class)
+    public Result creaNuevoTablero(Long idUsuario) {
+        String connectedUserStr = session("connected");
+        Long connectedUser =  Long.valueOf(connectedUserStr);
+        if (connectedUser != idUsuario) {
+            return unauthorized("Lo siento, no estás autorizado");
+        } else {
+            Form<Tablero> tableroForm = formFactory.form(Tablero.class).bindFromRequest();
+            if (tableroForm.hasErrors()) {
+                Usuario usuario = usuarioService.findUsuarioPorId(idUsuario);
+                return badRequest(formNuevoTablero.render(usuario, formFactory.form(Tablero.class), "Hay errores en el formulario"));
+            }
+            Tablero tablero = tableroForm.get();
+            tableroService.nuevoTablero(idUsuario, tablero.getNombre());
+            flash("aviso","El tablero se ha grabado correctamente");
+            return redirect(controllers.routes.GestionTablerosController.listarMisTableros(idUsuario));
+        }
     }
-  }
 
-  @Security.Authenticated(ActionAuthenticator.class)
-  public Result listarTableros(Long idUsuario){
-    String connectedUserStr = session("connected");
-    Long connectedUser = Long.valueOf(connectedUserStr);
-    if(connectedUser != idUsuario) {
-      return unauthorized("Lo siento, no estas autorizado");
-    } else {
-      String aviso = flash("aviso");
-      Usuario usuario = usuarioService.findUsuarioPorId(idUsuario);
-      List<Tablero> tablerosADMIN = tableroService.allTablerosAdministradosUser(idUsuario);
-      List<Tablero> tablerosJOINED = tableroService.allTablerosParticipadosUser(idUsuario);
-      List<Tablero> tablerosFREE = tableroService.allTablerosNoApuntadosUser(idUsuario);
-      return ok(listadoTableros.render(tablerosADMIN, tablerosJOINED, tablerosFREE, usuario, aviso));
+    @Security.Authenticated(ActionAuthenticator.class)
+    public Result listarMisTableros(Long idUsuario){
+        String connectedUserStr = session("connected");
+        Long connectedUser = Long.valueOf(connectedUserStr);
+        if(connectedUser != idUsuario) {
+            return unauthorized("Lo siento, no estas autorizado");
+        } else {
+            String aviso = flash("aviso");
+            Usuario usuario = usuarioService.findUsuarioPorId(idUsuario);
+            List<Tablero> tablerosADMIN = tableroService.allTablerosAdministradosUser(idUsuario);
+            List<Tablero> tablerosJOINED = tableroService.allTablerosParticipadosUser(idUsuario);
+            return ok(listadoMisTableros.render(tablerosADMIN, tablerosJOINED, usuario, aviso));
+        }
     }
-  }
 
-  @Security.Authenticated(ActionAuthenticator.class)
-  public Result añadirParticipante(Long idUsuario,Long idTablero){
-    String connectedUserStr = session("connected");
-    Long connectedUser = Long.valueOf(connectedUserStr);
-    if(connectedUser != idUsuario) {
-      return unauthorized("Lo siento, no estas autorizado");
-    } else {
-      Tablero tablero = tableroService.encontrarTablero(idTablero);
-      if(tablero==null) return notFound("Tablero no encontrado");
-      tableroService.añadirParticipante(idUsuario,idTablero);
-      return redirect(controllers.routes.GestionTablerosController.listarTableros(idUsuario));
-
+    @Security.Authenticated(ActionAuthenticator.class)
+    public Result listarOtrosTableros(Long idUsuario){
+        String connectedUserStr = session("connected");
+        Long connectedUser = Long.valueOf(connectedUserStr);
+        if(connectedUser != idUsuario) {
+            return unauthorized("Lo siento, no estas autorizado");
+        } else {
+            String aviso = flash("aviso");
+            Usuario usuario = usuarioService.findUsuarioPorId(idUsuario);
+            List<Tablero> tablerosFREE = tableroService.allTablerosNoApuntadosUser(idUsuario);
+            return ok(listadoOtrosTableros.render(tablerosFREE, usuario, aviso));
+        }
     }
-  }
 
-  @Security.Authenticated(ActionAuthenticator.class)
-  public Result mostrarDetalleTablero(Long idUsuario, Long idTablero){
-    String connectedUserStr = session("connected");
-    Long connectedUser = Long.valueOf(connectedUserStr);
-    if(connectedUser != idUsuario) {
-      return unauthorized("Lo siento, no estas autorizado");
-    } else {
-      Tablero tablero = tableroService.encontrarTablero(idTablero);
-      if(tablero==null) return notFound("Tablero no encontrado");
-      List<Usuario> lista = new ArrayList<Usuario>();
-      lista.addAll(tablero.getParticipantes());
-      return ok(desgloseTablero.render(tablero,lista, idUsuario));
+    @Security.Authenticated(ActionAuthenticator.class)
+    public Result añadirParticipante(Long idUsuario,Long idTablero){
+        String connectedUserStr = session("connected");
+        Long connectedUser = Long.valueOf(connectedUserStr);
+        if(connectedUser != idUsuario) {
+            return unauthorized("Lo siento, no estas autorizado");
+        } else {
+            Tablero tablero = tableroService.encontrarTablero(idTablero);
+            if(tablero==null) return notFound("Tablero no encontrado");
+            tableroService.añadirParticipante(idUsuario,idTablero);
+            return redirect(controllers.routes.GestionTablerosController.listarMisTableros(idUsuario));
+
+        }
     }
-  }
+
+    @Security.Authenticated(ActionAuthenticator.class)
+    public Result mostrarDetalleTablero(Long idUsuario, Long idTablero){
+        String connectedUserStr = session("connected");
+        Long connectedUser = Long.valueOf(connectedUserStr);
+        if(connectedUser != idUsuario) {
+            return unauthorized("Lo siento, no estas autorizado");
+        } else {
+            Tablero tablero = tableroService.encontrarTablero(idTablero);
+            if(tablero==null) return notFound("Tablero no encontrado");
+            List<Usuario> lista = new ArrayList<Usuario>();
+            lista.addAll(tablero.getParticipantes());
+            return ok(desgloseTablero.render(tablero,lista, idUsuario));
+        }
+    }
 
 }
