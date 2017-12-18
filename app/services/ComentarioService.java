334c6b44491bbbf66773ed1a056fd210dfe40728
= <trabajosadrianmr@gmail.com>
2017-12-18 22:40:14 +0100

    Añadidos metodos al Service

--- app/services/ComentarioService.java
+++ app/services/ComentarioService.java
@@ -9,6 +9,7 @@ import models.TareaRepository;
 import models.Comentario;
 import models.ComentarioRepository;
 
+import java.util.List;
 
 public class ComentarioService {
   UsuarioRepository usuarioRepository;
@@ -23,18 +24,38 @@ public class ComentarioService {
   }
 
 	public List<Comentario> findAllComentsByTareaId(Long idTarea){
-		return tareaRepository.findAllComentsByTarea(idTarea);
+		return comentarioRepository.findAllComentsByTarea(idTarea);
 	}
 
-	public Comentario crearComentario(Long idTarea,Long idUsuario,String comentario, String url){
+	public Comentario crearComentario(Long idTarea,Long idUsuario,String msg, String url){
 		Usuario usuario = usuarioRepository.findById(idUsuario);
 		if(usuario==null)
 			throw new ComentarioServiceException("No existe el usuario");
 		Tarea tarea = tareaRepository.findById(idTarea);
 		if(tarea==null)
 			throw new ComentarioServiceException("No existe la tarea");
-		Comentario comentario = new Comentario(tarea,usuario,comentario,url);
-		return comentarioRepository.create(comentario)
+		Comentario comentario = new Comentario(tarea,usuario,msg,url);
+		return comentarioRepository.create(comentario);
 	}
 
+	public Comentario obtenerComentario(Long idComentario){
+		return comentarioRepository.findById(idComentario);
+	}
+
+	public Comentario modificaComentario(Long idComentario, String nuevoMensaje, String nuevaURL){
+		Comentario comentario = comentarioRepository.findById(idComentario);
+		if(comentario==null)
+			throw new ComentarioServiceException("No existe la tarea");
+		comentario.setMensaje(nuevoMensaje);
+		comentario.setURL(nuevaURL);
+		comentario = comentarioRepository.update(comentario);
+		return comentario;
+	}
+
+	public void borrarComentario(Long idComentario){
+		Comentario comentario = comentarioRepository.findById(idComentario);
+		if(comentario==null)
+			throw new ComentarioServiceException("No existe la tarea");
+		comentarioRepository.delete(idComentario);
+	}
 }
