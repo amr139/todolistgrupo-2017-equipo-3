0e7d8db76d3bfc249eee899732ded422b0c6ec8b
= <trabajosadrianmr@gmail.com>
2017-12-20 00:39:57 +0100

    Implementado crear notificacion al crear comentario

--- app/controllers/ComentarioController.java
+++ app/controllers/ComentarioController.java
@@ -16,6 +16,8 @@ import java.text.SimpleDateFormat;
 import services.UsuarioService;
 import models.Usuario;
 import models.Tarea;
+import models.Columna;
+import models.Tablero;
 import models.Comentario;
 import models.Notificacion;
 import services.ComentarioService;
@@ -33,6 +35,7 @@ public class ComentarioController extends Controller {
 	@Inject FormFactory formFactory;
 	@Inject ComentarioService comentarioService;
 	@Inject TareaService tareaService;
+	@Inject UsuarioService usuarioService;
 	@Inject NotificacionService notificacionService;
 
 	@Security.Authenticated(ActionAuthenticator.class)
@@ -56,12 +59,28 @@ public class ComentarioController extends Controller {
 			Tarea tarea = tareaService.obtenerTarea(idTarea);
 			List<Comentario> vacia = new ArrayList<Comentario>();
           	return badRequest(listarComentarios.render(tarea,vacia,idUser, "Hay errores en el formulario"));
-       }
-	   DynamicForm requestData = formFactory.form().bindFromRequest();
-	   String mensaje = requestData.get("comment");
-	   Comentario c = comentarioService.crearComentario(idTarea,idUser,mensaje,"none");
-	   Tarea t  = tareaService.obtenerTarea(idTarea);
-	   Notificacion n = notificacionService.crearNotificacion(idUser, "Ha comentado el usuario "+t.getUsuario().getLogin()+" en la tarea: " + t.getTitulo());
+       	}
+	   	DynamicForm requestData = formFactory.form().bindFromRequest();
+	   	String mensaje = requestData.get("comment");
+	   	Comentario c = comentarioService.crearComentario(idTarea,idUser,mensaje,"none");
+
+		Usuario crea = usuarioService.findUsuarioPorId(idUser);
+
+	   	Tarea t  = tareaService.obtenerTarea(idTarea);
+	   	Columna col = t.getColumna();
+	   	Tablero ta = col.getTablero();
+	   	List<Usuario> participa = new ArrayList<Usuario>();
+		participa.addAll(ta.getParticipantes());
+
+	   	for(Usuario user : participa) {
+			if(user.getId()!=idUser){
+				Notificacion n = notificacionService.crearNotificacion(user.getId(), "Ha comentado el usuario "+crea.getLogin()+" en la tarea: " + t.getTitulo());
+			}
+	   	}
+		Usuario admin = ta.getAdministrador();
+		if(admin.getId()!=idUser){
+			Notificacion n = notificacionService.crearNotificacion(admin.getId(), "Ha comentado el usuario "+crea.getLogin()+" en la tarea: " + t.getTitulo());
+		}
 
 	   flash("aviso","El comentario se ha grabado correctamente");
 	   return redirect(controllers.routes.ComentarioController.ListarComentarios(idUser,idTarea));
